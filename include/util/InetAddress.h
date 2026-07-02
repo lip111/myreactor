75e63823620ff10f9c459d14629a30f63eac4a29
nishikata <1812077328@qq.com>
2026-07-02 21:25:23 +0800

    feat(InetAddress): implement IP+port wrapper for reactor

---
+++ include/util/InetAddress.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <string>
+
+namespace myreactor {
+
+class InetAddress {
+
+public:
+    // 构造函数
+    explicit InetAddress(uint16_t port, bool loopbackOnly=false);
+    InetAddress(const std::string& ip, uint16_t port); 
+    explicit InetAddress(const sockaddr_in& addr);
+
+    // 查询
+    std::string toIp() const;
+    uint16_t toPort() const { return ntohs(addr_.sin_port); }
+    std::string toIpPort() const;
+
+    const sockaddr* getSockAddr() const { return reinterpret_cast<const sockaddr*>(&addr_); }
+    sa_family_t family() const { return addr_.sin_family; }
+    socklen_t socklen() const { return sizeof(addr_); }
+
+    // 设置
+    void setIp(const std::string& ip);
+    void setPort(uint16_t port) { addr_.sin_port = htons(port); }
+
+
+
+private:
+    sockaddr_in addr_;
+} 
+
+
+;
+
+}
---
+++ src/util/InetAddress.cc
@@ -0,0 +1,44 @@
+#include "InetAddress.h"
+#include <arpa/inet.h>
+
+namespace myreactor {
+
+// 构造函数
+InetAddress::InetAddress(uint16_t port, bool loopbackOnly): addr_{} {   // 初始化列表中进行值初始化
+    addr_.sin_family = AF_INET;
+    addr_.sin_port = htons(port);
+    addr_.sin_addr.s_addr = htons(loopbackOnly ? INADDR_LOOPBACK: INADDR_ANY);
+}
+
+InetAddress::InetAddress(const std::string& ip, uint16_t port): addr_{} {
+    addr_.sin_family = AF_INET;
+    addr_.sin_port = htons(port);
+    if (inet_pton(AF_INET, ip.c_str(), &addr_.sin_addr) <= 0) {
+        addr_.sin_addr.s_addr = htons(INADDR_ANY);
+    } 
+}
+
+InetAddress::InetAddress(const sockaddr_in& addr): addr_(addr) {}
+
+
+// 查询
+std::string InetAddress::toIp() const {
+    char buf[64] = {0};
+    inet_ntop(AF_INET, &addr_.sin_addr, buf, sizeof(buf));
+    return buf;
+}
+
+std::string InetAddress::toIpPort() const {
+    return toIp() + ":" + std::to_string(toPort());
+}
+
+// 设置
+void InetAddress::setIp(const std::string& ip) {
+    inet_pton(AF_INET, ip.c_str(), &addr_.sin_addr);
+}
+
+
+
+
+;
+}
